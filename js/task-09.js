18ceb66e704057c10aab7617eccd093fa542031b
Nikolay Bohdanov <bohdanovmykola@gmail.com>
2023-07-11 19:08:33 +0300

    Task 9 Done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const buttonEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color")
+buttonEl.addEventListener("click", setRandomHexColor)
+function setRandomHexColor(event) {
+  const hexColour = getRandomHexColor();
+  spanEl.textContent = hexColour;
+  body.style.backgroundColor = hexColour;
+}
