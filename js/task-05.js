d67fa407d57c414ea314e9a4ee485cd5b703c22c
Nikolay Bohdanov <bohdanovmykola@gmail.com>
2023-07-12 14:39:29 +0300

    Fix mistakes

--- js/task-05.js
+++ js/task-05.js
@@ -5,5 +5,5 @@ const refs = {
 console.dir(refs.inputEl);
 refs.inputEl.addEventListener("input", changeSpanValue)
 function changeSpanValue(event) {
-    event.currentTarget.value ? refs.outputSpan.textContent = event.currentTarget.value : refs.outputSpan.textContent = "Anonymous"
+    event.currentTarget.value.trim() ? refs.outputSpan.textContent = event.currentTarget.value : refs.outputSpan.textContent = "Anonymous"
 }
